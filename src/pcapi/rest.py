440495674e0b5885b0b11e0b3229353d84f2cea2
Michael Koutroumpas <m.koutroumpas@ed.ac.uk>
2014-09-24 16:10:47 +0300

    Reenable records PUT.

--- src/pcapi/rest.py
+++ src/pcapi/rest.py
@@ -229,35 +229,15 @@ class PCAPIRest(object):
         try:
             recordname_lst = re.findall("/records//?([^/]*)$", path)
             if recordname_lst:
-                # We have a depth 1 e.g. /records/myrecord or just /records/
                 if self.request.method == "PUT":
-                    ### This case if for moving a record folder to another folder
-                    #return { "error": 1, "msg": "PUT request needs 2 level paths e.g.  \
-                    #/records/level1/filename instead of %s" % path}
-                    #log.debug(self.request.body.read())
-                    path2 = "/records/%s" % self.request.body.read()
-                    log.debug(path2)
-                    md = self.provider.move(path, path2)
-                    log.debug(md)
-                    if md.path() != path:
-                        newname = md.path()[md.path().rfind("/") + 1:]
-                        def proc(fp):
-                            j = json.loads(fp.read())
-                            j["name"]=newname
-                            log.debug("Name collision. Renamed record to: " + newname)
-                            return StringIO(json.dumps(j))
-                        cb = proc
-                    else:
-                        cb = None
-                    path = md.path() + "/record.json"
-                    #return self.fs(provider, userid, path, cb)
-                    #log.debug(md.ls())
-                    return { "error": 0, "msg" : "File uploaded", "path":md.ls()}
+                    ## NOTE: Put is *not* currently used by FTOPEN
+                    return self.fs(provider, userid, path)
                 if self.request.method == "POST":
                     ## We are in depth 1. Create directory (or rename directory) and then upload record.json
                     md = self.provider.mkdir(path)
                     # check path is different and add a callback to update the record's name
                     if ( md.path() != path ):
+                        ### moved a myrecord/record.json to a new folder anothername/record.json
                         newname = md.path()[md.path().rfind("/") + 1:]
                         def proc(fp):
                             j = json.loads(fp.read())
@@ -407,8 +387,7 @@ class PCAPIRest(object):
                                 return { "error": 1, "msg" : "The editor is not valid"}
             ######## PUT -> Upload/Overwrite file using dropbox rules ########
             if method=="PUT":
-                # if process is defined then pipe the body through process
-                fp = self.request.body if not process else process(self.request.body)
+                fp = self.request.body
                 md = self.provider.upload(path, fp, overwrite=True)
                 return { "error": 0, "msg" : "File uploaded", "path":md.ls()}
             ######## POST -> Upload/Rename file using dropbox rules ########
